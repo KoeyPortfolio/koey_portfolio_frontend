b23302cad087405084e5b0d6d111ada36bb15ef5
koeyismyusername <koey2182@gmail.com>
2025-02-27 15:21:05 +0900

    경력사항 타임라인뷰로 리팩토링

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
 import ExperienceRepository from "../../lib/repository/experience_repository";
+import TimeLineView from "./TimeLineView";
 
 export default function Experience() {
   const [experiences, setExperiences] = useState([]);
@@ -15,13 +16,38 @@ export default function Experience() {
 
   return (
     <>
-      <ExperienceList>
+      <TimeLineView>
         {
           experiences.map((x, i) =>
-            <ExperienceList.Item key={i} company={x.company} department={x.department} jobTitle={x.jobTitle} start={dayjs(x.start).format('YYYY.MM')} end={dayjs(x.end).format('YYYY.MM')} details={x.details} />
+            <TimeLineView.Item key={i}>
+              <TimeLineView.Item.Left>
+                <div className="text-lg font-bold">
+                  <div className="me-6">
+                    { dayjs(x.start).format('YYYY.MM') }
+                  </div>
+                  <div>
+                    ~ { x.end ? dayjs(x.end).format('YYYY.MM') : '현재' }
+                  </div>
+                </div>
+              </TimeLineView.Item.Left>
+              <TimeLineView.Item.Right>
+                <h3 className="font-bold text-xl/12 before:content-['🏢'] before:mr-1">{ x.company }</h3>
+                <div className="flex ms-4 gap-2 mb-4">
+                  <JobTag>{ x.department }</JobTag>
+                  <JobTag>{ x.jobTitle }</JobTag>
+                </div>
+                <ol>
+                  {
+                    x.details.map((y, j) =>
+                      <li key={ j } className="text-base/6 list-disc my-2">{ y }</li>
+                    )
+                  }
+                </ol>
+              </TimeLineView.Item.Right>
+            </TimeLineView.Item>
           )
         }
-      </ExperienceList>
+      </TimeLineView>
     </>
   );
 }
@@ -40,7 +66,7 @@ function ExperienceList({ children = [] }) {
   );
 }
 
-ExperienceList.Item = function ExperienceListItem({ start, end, company, details = [], department, jobTitle}) {
+ExperienceList.Item = function({ start, end, company, details = [], department, jobTitle}) {
   const class1 = `text-lg/6 font-bold w-[25%] before:w-[10px] before:h-[10px] before:bg-indigo-900 before:rounded-[50%] before:absolute before:left-[25%] before:-translate-x-[4px] before:top-[20px] after:w-[16px] after:h-[16px] after:border-indigo-900 after:border-1 after:rounded-[50%] after:absolute after:top-[17px] after:left-[25%] after:-translate-x-[7px] text-end pe-10`;
 
   return (
@@ -75,7 +101,7 @@ ExperienceList.Item = function ExperienceListItem({ start, end, company, details
 
 function JobTag({ children }) {
   return (
-    <p className="flex items-center text-white px-2 bg-indigo-700 rounded-lg">
+    <p className="flex items-center text-indigo-600 px-2 bg-indigo-100 rounded-lg">
       { children }
     </p>
   );
