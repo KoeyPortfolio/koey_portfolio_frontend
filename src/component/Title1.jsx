693004de2d54eaa82d759fa12e3818e48dbdc893
koeyismyusername <koey2182@gmail.com>
2025-02-27 15:25:17 +0900

    Title1 컴포넌트 정리

--- src/component/Title1.jsx
+++ src/component/Title1.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Title1({ children, className }) {
+export default function Title1({ children }) {
   return(
     <>
-      <h1 className={`text-2xl my-10 font-bold ${className}`}>{ children }</h1>
+      <h1 className={'text-2xl my-10 font-bold'}>{ children }</h1>
     </>
   );
 }
