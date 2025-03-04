bd36d6b6252f6c6d91ae30cd23f1a3f4bf0f1bbc
koeyismyusername <koey2182@gmail.com>
2025-03-04 17:31:51 +0900

    프로젝트 상세 갤러리 리스트 뷰 타이틀 배경 투명도 높게 변경

--- src/common_component/GalaryListView.jsx
+++ src/common_component/GalaryListView.jsx
@@ -4,7 +4,7 @@ export default function GalaryListView({ items = [], onItemClick }) {
   const galaryListCss = "flex flex-wrap gap-y-2 gap-x-4 justify-center";
   const galaryItemCss = "max-w-75 max-h-75 flex flex-col justify-end items-center relative hover:cursor-pointer";
   const galaryItemImgCss = "block w-full h-full object-contain";
-  const galaryItemTitleCss = "bg-white/50 w-full absolute py-1 px-2";
+  const galaryItemTitleCss = "bg-white/80 w-full absolute py-1 px-2";
 
   return (
     <>
