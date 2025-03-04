1b4fb1ddd528a12ac1ea7d35c9d60e58653fc643
koeyismyusername <koey2182@gmail.com>
2025-03-04 11:02:56 +0900

    갤러리 뷰 컴포넌트 추가

--- src/component/GalaryView.jsx
+++ src/component/GalaryView.jsx
@@ -1,14 +1,36 @@
-import React, { Children } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function GalaryView({ children }) {
-  const items = Children.toArray(children).filter(x => x.type === GalaryView.Item);
-  
+export default function GalaryView({ imgUrls=[] }) {
+  const [mainImgUrl, setMainImgUrl] = useState(undefined);
+  const scrollContainerRef = useRef(null);
+
+  useEffect(bindMainImgUrl, [imgUrls]);
+
+  function bindMainImgUrl(url) {
+    const data = url ? url : imgUrls.find(() => true);
+
+    setMainImgUrl(data);
+  }
+
+  const galaryViewItemListCss = "flex overflow-auto";
+  const galaryViewItemCss = "w-[22%] aspect-[16/10] shrink-0";
+  const mainItemCss = "aspect-[16/10]";
+  const imgListCss = "w-full h-full block object-cover hover:cursor-pointer";
+  const mainImgCss = "w-full h-full block object-contain bg-gray-900";
+
   return (
     <>
+      <ul className={galaryViewItemListCss} ref={scrollContainerRef}>
+        {
+          imgUrls.map((x, i) =>
+            <li key={i} className={galaryViewItemCss}>
+              <img src={x} className={imgListCss} onClick={_ => bindMainImgUrl(x)}/>
+          </li>)
+        }
+      </ul>
+      <div className={mainItemCss}>
+        <img src={mainImgUrl} className={mainImgCss} />
+      </div>
     </>
   );
-}
-
-GalaryView.Item = function() {
-
 }
--- src/page/ProjectDetailPage.jsx
+++ src/page/ProjectDetailPage.jsx
@@ -17,58 +17,20 @@ export default function ProjectDetailPage() {
     setProjectDetail(data);
   }
 
-  const projectDetailPageTitleCss = "text-2xl font-bold my-10";
-
-  const galaryMainImgCss = "block w-full aspect-[calc(16/10)] object-contain";
-  const galaryPreviewImgListCss = "w-full flex overflow-auto";
-  const galaryPreviewImgCss = "block object-cover w-40 aspect-[calc(16/10)]";
-  const galaryPreviewImgItemCss = "shrink-0";
-  const galaryBtnCss = "w px-3 text-3xl text-black/0 font-bold hover:bg-gray-900/40 hover:text-gray-50/60 hover:cursor-pointer flex items-center justify-center";
+  const imgUrls = [
+    "https://images.unsplash.com/photo-1740149263431-098cc6705df4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1736618625357-2a7f197f8c23?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1737392402963-a489a17826eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1739996698574-30dfcdaa64b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://plus.unsplash.com/premium_photo-1736392070194-530edb275b36?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1740257862940-66edfed66a6a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D",
+    "https://plus.unsplash.com/premium_photo-1673139285535-80a03340beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"
+  ]
 
   return (
     <>
       <h2 className="text-2xl font-bold my-10">갤러리</h2>
-      <GalaryView>
-        <GalaryView.Item>
-          
-        </GalaryView.Item>
-      </GalaryView>
-      <div className="mx-auto max-w-200 min-w-120">
-        <ul className={galaryPreviewImgListCss}>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://images.unsplash.com/photo-1740149263431-098cc6705df4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://images.unsplash.com/photo-1737392402963-a489a17826eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://images.unsplash.com/photo-1739996698574-30dfcdaa64b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://plus.unsplash.com/premium_photo-1736392070194-530edb275b36?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://images.unsplash.com/photo-1736618625357-2a7f197f8c23?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://images.unsplash.com/photo-1740257862940-66edfed66a6a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-          <li className={galaryPreviewImgItemCss}>
-            <img className={galaryPreviewImgCss} src="https://plus.unsplash.com/premium_photo-1673139285535-80a03340beda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"/>
-          </li>
-        </ul>
-        <div className="bg-gray-900 flex flex-col relative justify-center">
-          <img className={galaryMainImgCss} src="https://images.unsplash.com/photo-1740149263431-098cc6705df4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D" />
-          <div className="absolute w-full h-full flex justify-between">
-            <button className={galaryBtnCss} type="button">
-              <i className="bi bi-arrow-left"></i>
-            </button>
-            <button className={galaryBtnCss} type="button">
-              <i className="bi bi-arrow-right"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <GalaryView imgUrls={imgUrls} />
     </>
   );
 }
