f219351f66f87aa46d1f41469d53ad1444663afc
koeyismyusername <koey2182@gmail.com>
2025-03-04 17:47:25 +0900

    갤러리 크게 보기 구현

--- src/page/ProjectDetailPage.jsx
+++ src/page/ProjectDetailPage.jsx
@@ -8,6 +8,7 @@ import GalaryListView from "../common_component/GalaryListView";
 export default function ProjectDetailPage() {
   const {id: projectId} = useParams();
   const [projectDetail, setProjectDetail] = useState(undefined);
+  const [detailImgUrl, setDetailImgUrl] = useState(undefined);
 
   useEffect(bindProjectDetail, [projectId]);
 
@@ -26,8 +27,18 @@ export default function ProjectDetailPage() {
         projectDetail && <GalaryView imgUrls={projectDetail.imgs.map(x => x.imgUrl)} />
       } */}
       {
-        projectDetail && <GalaryListView items={projectDetail.imgs} onItemClick={item => alert(item.imgUrl)} />
+        projectDetail && <GalaryListView items={projectDetail.imgs} onItemClick={item => setDetailImgUrl(item.imgUrl)} />
       }
+      {
+        detailImgUrl && 
+        <div className="fixed w-screen h-screen bg-gray-900/80 m-auto top-0 left-0 flex justify-center items-center">
+          <img src={detailImgUrl} className="h-[90%] w-[90%] object-contain" />
+          <button type="button" className="absolute bg-gray-900/50 text-gray-50/50 hover:bg-white hover:text-gray-900 right-[10%] top-[10%] aspect-square w-12 rounded-full hover:cursor-pointer" onClick={_ => setDetailImgUrl(undefined)}>
+            <i class="bi bi-x-lg"></i>
+          </button>
+        </div>
+      }
+      
       <Title1>사용한 기술</Title1>
     </>
   );
